73a579fcc56a3ee859d06e1862f141fc112fa512
agudavalli <146471079+agudavalli@users.noreply.github.com>
2026-02-18 19:11:53 -0500

    added a file for functions to modularize and improve readability

---
+++ functions.py
@@ -0,0 +1,15 @@
+from gtts import gTTS
+from mutagen.mp3 import MP3
+import os
+
+def readschedule():
+    file = 'test.mp3'
+    tts = gTTS(text="You have a meeting at 3 PM. Don't forget to prepare the presentation.", lang='en', slow=False)
+    tts.save(file)
+    os.system("afplay " + file)
+    return get_duration(file)
+
+def get_duration(file):
+    audio = MP3(file)
+    return audio.info.length
+readschedule()
