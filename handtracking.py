35f2682f4d451e3455492b391887622b56512fd4
agudavalli <146471079+agudavalli@users.noreply.github.com>
2026-02-21 04:36:42 -0500

    changed camera flag logic

--- handtracking.py
+++ handtracking.py
@@ -105,16 +105,19 @@ def main():
                         functions.take_picture(frame)
                         cv2.putText(frame, 'Picture Taken!', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,255), 2)
                         picture = True
+                        calendar_flag = False
 
                     elif finger_state == '11111' and song_flag:
                         functions.stop_song()
                         song_flag = False
                         picture = False
+                        calendar_flag = False
 
                     elif finger_state == '11001' and not song_flag:
                         functions.play_song()
                         song_flag = True
                         picture = False
+                        calendar_flag = False
 
                     elif finger_state == '00110' and not calendar_flag:
                         functions.readschedule()
@@ -124,16 +127,19 @@ def main():
                     elif finger_state == '00111':
                         functions.words_of_affirmation()
                         picture = False
+                        calendar_flag = False
 
                     elif finger_state == '01001' and not song_flag:
                         functions.play_flashing_lights()
                         song_flag = True
                         picture = False
+                        calendar_flag = False
 
                     elif finger_state == '10101' and not calibrated:
                         calibrated = True
                         functions.calibrate()
                         picture = False
+                        calendar_flag = False
 
 
                     #cv2.putText(frame, finger_state, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,0), 2)
