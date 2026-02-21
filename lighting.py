ff80e694c3c861886bf60e01b87fbf7a070e0973
agudavalli <146471079+agudavalli@users.noreply.github.com>
2026-02-21 05:14:36 -0500

    testing

--- lighting.py
+++ lighting.py
@@ -149,8 +149,8 @@ for line in sys.stdin:
     elif line == 'nether':
         nether()
     elif line == 'flashing_lights':
-        #flashing_lights_kanye()
-        leofunctions.flashing_lights_kanye(strip)
+        flashing_lights_kanye()
+        #leofunctions.flashing_lights_kanye(strip)
     elif line == 'camera':
         leofunctions.camera_flash(strip)
     elif line == 'affirmation':
