cbd2bd40a1ed7e466a2b32bac6c5b76c113a7dcb
ZhiwenLiuBU-ENG <zliu23@bu.edu>
2026-02-21 00:16:01 -0500

    Update ledcount.py

--- ledcount.py
+++ ledcount.py
@@ -7,5 +7,5 @@ BRIGHTNESS = 0.4
 strip = neopixel.NeoPixel(board.D18, NUM_LEDS, brightness=BRIGHTNESS, auto_write=False)
 
 # Light up LED 140 in white
-strip[140] = (255, 255, 255)
+strip[135] = (255, 255, 255)
 strip.show()
