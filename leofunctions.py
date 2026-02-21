ac25a346c36d418381ff8d4d303f482870c039df
agudavalli <146471079+agudavalli@users.noreply.github.com>
2026-02-21 05:44:44 -0500

    hello

--- leofunctions.py
+++ leofunctions.py
@@ -212,6 +212,7 @@ def gamecube_intro(strip):
 
 def piano_scale_egg(strip):
     import random
+    time.sleep(3.8)
 
     # --- Adjustable timing ---
     BPM           = 153  # adjust to match video
