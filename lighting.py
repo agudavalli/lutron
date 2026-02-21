6e6c108a1b123fa3501e2aa96ac6409e768f3dae
agudavalli <146471079+agudavalli@users.noreply.github.com>
2026-02-21 01:59:49 -0500

    changed nether

--- lighting.py
+++ lighting.py
@@ -103,7 +103,9 @@ def all_yellow():
     strip.show()
 
 def nether():
-    strip.fill((97,0,178))
+    strip.fill((60,0,110))
+    #strip.fill((97,0,178))
+    #strip.fill((150,50,255))
     strip.show()
 
 
